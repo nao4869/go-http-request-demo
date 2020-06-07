5b5d788fe7a9a65bb85bdc33f7abd8d600fd10fe
naohito muraoka <muraokan@oregonstate.edu>
2020-06-07 21:19:23 +0900

    Added: custom logWrite function added for interface demo

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
-	repsponse, error := http.Get("http://google.com")
+	repsponse, error := http.Get("htrestp://google.com")
 
 	if error != nil {
 		fmt.Println("Error:", error)
 		os.Exit(1)
 	}
-	fmt.Println(repsponse)
+	//fmt.Println(repsponse)
+
+	// empty byte slice which have 99999 spaces
+	// bs := make([]byte, 99999)
+	// repsponse.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+
+	// same as above 3 lines
+	// func Copy(dst Writer, src Reader) (written int64, err error)
+	io.Copy(lw, repsponse.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println(len(bs))
+	return len(bs), nil
 }
